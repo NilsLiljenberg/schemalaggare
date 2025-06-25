aa315b78516cac9efae54a440feadf0268898c04
NilsLiljenberg <nils.liljenberg@gmail.com>
2025-06-25 11:31:45 +0200

    Create balances.js

---
+++ src/balances.js
@@ -0,0 +1,31 @@
+const balances = [
+  "Maskering TS",
+  "Kontrollering 1",
+  "Avs 1",
+  "Avs 2",
+  "Avs 3",
+  "Nedre Förlast",
+  "Nedre Pålast",
+  "Nedre VTC",
+  "Nedre Avs",
+  "SLUTKONTROLL",
+  "Sillar",
+  "Avplock",
+  "Takspoiler"
+];
+
+export const colorMap = {
+  "Avs 1": "FFFF00",
+  "Avs 2": "FFFF00",
+  "Avs 3": "FFFF00",
+  "Nedre Förlast": "808000",
+  "Nedre Pålast": "808000",
+  "Nedre VTC": "808000",
+  "Nedre Avs": "808000",
+  "SLUTKONTROLL": "FFA500",
+  "Sillar": "ADD8E6",
+  "Avplock": "ADD8E6",
+  "Takspoiler": "ADD8E6"
+};
+
+export default balances;
